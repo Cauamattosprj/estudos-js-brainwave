bc2bfcfbcf170f94bdedcd40fa46af031f363224
Caua Mattos <caua.pereira@flybank.com.br>
2024-10-30 16:16:23 -0300

    Day 1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from "./components/Button"
+import Header from "./components/Header"
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+
+      <h1 className='text-3xl font-bold underline'>Teste h1</h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        
+      </div>
+
+      <ButtonGradient />
+    </>
+  )
+}
+
+export default App
